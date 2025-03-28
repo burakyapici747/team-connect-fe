4217ee74b459ccc88cd07af4b3fd9e0a56b19171
burakyapici <burakyapici747@gmail.com>
2025-03-28 23:41:24 +0300

    feat: add file attachment support to messaging API

--- src/features/messages/api/index.ts
+++ src/features/messages/api/index.ts
@@ -2,6 +2,7 @@ import { ApiResponse } from "@/shared/api/response/response";
 import { MessageOutput } from "@/features/messages/api/output/MessageOutput";
 import { getAllWithPathVariable, postSingleWithPathVariable } from "@/shared/api/genericAPI";
 import { API_ENDPOINTS } from "@/core/config/axios/api-endpoints";
+import {MessageSendInput} from "@/features/messages/api/input/MessageSendInput";
 
 export const getMessagesByChannelId = async (
   channelId: string,
@@ -27,14 +28,15 @@ export const getMessagesByChannelIdWithBefore = async (
   );
 };
 
-export const sendMessage: <T extends object>(
+export const sendMessage: (
   channelId: string,
-  requestBody: T
-) => Promise<ApiResponse<MessageOutput>> = async <T extends object>(
+  requestBody: MessageSendInput
+) => Promise<ApiResponse<MessageOutput>> = async (
   channelId: string,
-  requestBody: T
+  requestBody: MessageSendInput
 ): Promise<ApiResponse<MessageOutput>> => {
-  return postSingleWithPathVariable<T, MessageOutput>(
+  debugger;
+  return postSingleWithPathVariable<MessageSendInput, MessageOutput>(
     "",
     requestBody,
     API_ENDPOINTS.MESSAGES.SEND_MESSAGE,
